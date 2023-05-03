96660f2044940e132e941708ed8c5dd2ad3a3971
Davi Souza de Luna <Sdavi738@gmail.com>
2023-05-03 01:45:05 -0300

    adicionando a rota para o create

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ProdutosController extends Controller
 {
-    public function create(){
+    public function create(){ //função que realiza a operação create
         return view('produtos.create');
     }
 }
