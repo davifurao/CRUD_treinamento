1d8ac9fe13851549691a967de8255aa35344df01
Davi Souza de Luna <Sdavi738@gmail.com>
2023-05-03 01:41:35 -0300

    Adicionando o controller e a view da operação create

---
+++ resources/views/create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cadastro de um novo produto</title>
+</head>
+<body>
+    <form action="">
+        <label for="">Nome</label><br/>
+        <input type="text" name="nome"><br/>
+        <label for="">Custo</label><br/>
+        <input type="text" name="custo"><br/>
+        <label for="">Preço</label><br/>
+        <input type="text" name="preco"><br/>
+        <label for="">Quantidade</label><br/>
+        <input type="text" name="quantidade"><br/>
+    </form>
+</body>
+</html>
